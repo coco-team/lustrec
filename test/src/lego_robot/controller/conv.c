94fb84c31ca8a3dbb1e102deea6fab5384c29805
ploc <ploc@041b043f-8d7c-46b2-b46e-ef0dd855326e>
2014-02-05 14:03:37 +0000

    lego robot example
    git-svn-id: https://cavale.enseeiht.fr/svn/lustrec/lustre_compiler/trunk@140 041b043f-8d7c-46b2-b46e-ef0dd855326e

---
+++ test/src/lego_robot/controller/conv.c
@@ -0,0 +1,15 @@
+int real_to_int (double in1, int *out)
+{
+    *out = (int)in1;
+
+    return *out;
+}
+
+double int_to_real (int in1, double *out)
+{
+    *out = (double)in1;
+
+    return *out;
+}
+
+
