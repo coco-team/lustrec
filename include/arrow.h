2e6f9ba8e990ffc9345f2c8cf60f6df6bfe4a8bf
xthirioux <xthirioux@041b043f-8d7c-46b2-b46e-ef0dd855326e>
2014-03-17 12:01:34 +0000

    improved code generation by factorizing out arrows
    git-svn-id: https://cavale.enseeiht.fr/svn/lustrec/lustre_compiler/trunk@207 041b043f-8d7c-46b2-b46e-ef0dd855326e

--- include/arrow.h
+++ include/arrow.h
@@ -21,4 +21,8 @@ extern struct _arrow_mem *_arrow_alloc ();
 
 #define _arrow_reset(self) {(self)->_reg._first = 1;}
 
+/* Step macro for specialized arrows of the form: (true -> false) */
+
+#define _once_step(output,self) { *output = (self)->_reg._first; if ((self)->_reg._first) { (self)->_reg._first=0; }; }
+
 #endif
