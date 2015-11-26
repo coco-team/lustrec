12738191821ccbe174c69675a1eb4776b8ad4ac2
xthirioux <xthirioux@041b043f-8d7c-46b2-b46e-ef0dd855326e>
2015-11-26 15:49:15 +0000

    ....

---
+++ src/global.ml
@@ -0,0 +1,21 @@
+let type_env : (Types.type_expr Env.t) ref = ref Env.initial
+let clock_env : (Clocks.clock_expr Env.t) ref = ref Env.initial
+let basename = ref ""
+let main_node = ref ""
+
+module TypeEnv =
+struct
+let lookup_value ident = Env.lookup_value !type_env ident
+let exists_value ident = Env.exists_value !type_env ident
+let iter f = Env.iter !type_env f
+let pp pp_fun fmt () = Env.pp_env pp_fun fmt !type_env
+end
+
+let initialize () =
+  begin
+    main_node := !Options.main_node;
+  end
+
+(* Local Variables: *)
+(* compile-command:"make -C .." *)
+(* End: *)
