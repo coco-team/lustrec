042dc5863c878be36eaef745c1e23a0c696cbf44
ploc <ploc@garoche.net>
2017-01-11 10:02:16 +0100

    Revert some ocaml code to ocaml 4.01 compatibility

--- src/backends/Horn/horn_backend.ml
+++ src/backends/Horn/horn_backend.ml
@@ -52,7 +52,7 @@ end
 let load_file f =
   let ic = open_in f in
   let n = in_channel_length ic in
-  let s = Bytes.create n in
+  let s = String.create n in
   really_input ic s 0 n;
   close_in ic;
   (s)
