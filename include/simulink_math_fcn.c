616ee79e6fac929f5b1d278ba29f9654ebcc8111
Bourbouh <hamza.bourbouh@nasa.gov>
2018-07-23 18:06:22 -0700

    fix mod and rem in simulink_math library

--- include/simulink_math_fcn.c
+++ include/simulink_math_fcn.c
@@ -19,14 +19,19 @@ int mod_int (int x, int y) {
 }
 
 double rem_real (double x, double y) {
-  return fmod(x, y);
+    if (x == 0.0 || y == 0.0){
+        return 0.0;
+    }else{
+        return fmod(x, y);
+    }
 }
 
 double mod_real (double x, double y) {
   double tmp = 0.;
   if (y == 0.) { return x; };
+  if (x == 0.) { return 0; };
   tmp = fmod(x, y);
-  if (y < 0. && tmp > 0.) {
+  if (y*tmp < 0.) {
     return tmp+y;
   }
   else {
