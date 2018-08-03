e4811e4c6fec1369742e7a5b47c2715fe3058b15
Bourbouh <hamza.bourbouh@nasa.gov>
2018-08-03 15:58:25 -0700

    add more conversion libraries

--- include/conv.c
+++ include/conv.c
@@ -1,3 +1,10 @@
+#include <math.h>
 int real_to_int (double in1) { return (int)in1; }
+int _Floor (double in1) { return (int)floor(in1); }
+double _floor (double in1) { return  floor(in1); }
+int _Ceiling (double in1) { return (int)ceil(in1); }
+double _ceil (double in1) { return  ceil(in1); }
+int _Round (double in1) { return (int)round(in1); }
+double _round (double in1) { return round(in1); }
 
 double int_to_real (int in1) { return (double)in1; }
